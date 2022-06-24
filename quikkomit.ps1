28f3bcd08e6e3ef39d1729c73d70d2538b4e8971
gamesBrown <studio13productions@outlook.com>
2022-06-24 13:41:25 -0700

    ikool

--- quikkomit.ps1
+++ quikkomit.ps1
@@ -2,4 +2,4 @@
 
 git add .
 git commit
-git push -p htps://gamesBrown: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@github.com
+git push htps://gamesBrown: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@github.com
