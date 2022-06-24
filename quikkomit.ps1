1cec804266aaf135baf844ded0832028810b13fa
gamesBrown <studio13productions@outlook.com>
2022-06-24 13:40:49 -0700

    ikoolbeans

--- quikkomit.ps1
+++ quikkomit.ps1
@@ -1,9 +1,5 @@
 ﻿
 
-$repo_name= Read-Host -Prompt "Enter Project Nam"
-$comment = Read-Host -Prompt "Enter First Commit Comment"
-
-
 git add .
-git commit -m $comment
-git push -p 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
+git commit
+git push -p htps://gamesBrown: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@github.com
