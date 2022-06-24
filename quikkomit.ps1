9d53603ec0f5ee779f71bac8f0000a24765ded31
gamesBrown <studio13productions@outlook.com>
2022-06-24 13:37:50 -0700

    k

--- quikkomit.ps1
+++ quikkomit.ps1
@@ -6,4 +6,4 @@ $comment = Read-Host -Prompt "Enter First Commit Comment"
 
 git add .
 git commit -m $comment
-git push origin -m main https://gamesBrown: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@github.com/gamesBrown/$repo_name.git
+git push -p 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
